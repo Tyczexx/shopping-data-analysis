d478455014c8522c6f40335db3de3631bf812ac7
Tyczexx <155094045+Tyczexx@users.noreply.github.com>
2025-03-23 15:08:35 +0100

    Update top_customers_per_category.sql

--- top_customers_per_category.sql
+++ top_customers_per_category.sql
@@ -1,4 +1,5 @@
--- top 3 customers who purchased the most in each category, where total purchase amount for category was more than 30,000 USD
+/* top 3 customers who purchased the most in each category
+condition: total sales amount for a category was more than 30,000 USD */
 with category_sales as 
 ( 
 select 
@@ -26,3 +27,7 @@ select
 	,rc.nb_of_purchases
 from ranked_customers rc
 where rank_in_category <=3;
+
+/* The result: Based on first CTE, total sales amount with the value more than 30,000 USD, was noticed for 3/4 of main categories.
+Second CTE displayed ranking of customers according to how many orders they have placed in those categories. 
+The final SELECT shows top 3 customers. It would be good to consider if we should reward them for kind of commitment. */
