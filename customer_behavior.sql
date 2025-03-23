30119af116f64a07864a4afd9acbafdd55794787
Tyczexx <155094045+Tyczexx@users.noreply.github.com>
2025-03-23 14:41:15 +0100

    Update customer_behavior.sql

--- customer_behavior.sql
+++ customer_behavior.sql
@@ -1,12 +1,16 @@
 select * from shopping_data
 
--- customers who bought anything in summer season
+-- adult customers who bought anything in summer season
 select 
 	count(customer_id) as customers
 	,season 
 from shopping_data
-where age > 30 
+where age > 30 and season = 'Summer'
 group by season;
+
+/*The result: there were 713 "adult" customers made purchases during summer period. 
+However, comparing with other seasons, summer was the worst-trending one.*/
+
 
 -- total sales amount for each product category and item
 select 
@@ -16,3 +20,4 @@ select
 from shopping_data
 group by 2,3
 order by 1 desc;
+--The result: There was observed most sales on Clothing category and Blouses as a product item (10 410 USD)
